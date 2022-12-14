fe7961efb7c01f51a15a140637cbb9c558532dee
mohamidsaiid <100053790+mohamidsaiid@users.noreply.github.com>
2022-12-14 19:30:50 +0200

    Create Richest Customer Wealth.cpp

---
+++ Week 1/Richest Customer Wealth.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int wealth = 0;
+        int maxx = 0;
+        for (int i = 0; i < 1; i++)
+        {
+            for (int j = 0; j < accounts[i].size(); j++)
+            {
+                maxx += accounts[i][j];
+            }
+        }
+        for (int i = 0; i < accounts.size(); i++)
+        {
+            
+            for (int j = 0; j < accounts[i].size(); j++)
+            {
+                wealth += accounts[i][j];
+            }
+            if (maxx < wealth)
+            {
+                maxx = wealth;
+            }
+            wealth = 0;
+        }
+        //int maxx = max(wealth,wealth + wealth.size());
+        return maxx;
+    }
+};
