36c7e393febe071691bbe9183aadf16bc5860b98
mohamidsaiid <100053790+mohamidsaiid@users.noreply.github.com>
2022-12-16 02:45:57 +0200

    Create Majority Element.cpp

---
+++ Week 1/Majority Element.cpp
@@ -0,0 +1,10 @@
+/*
+Problem Name : Majority Element
+*/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
+    }
+};
