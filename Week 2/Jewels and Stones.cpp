059c77ce68bc3fabe3b5843f12e58878184b90ea
mohamidsaiid <100053790+mohamidsaiid@users.noreply.github.com>
2023-02-11 12:56:20 +0200

    Update Jewels and Stones.cpp

--- Week 2/Jewels and Stones.cpp
+++ Week 2/Jewels and Stones.cpp
@@ -5,18 +5,14 @@ class Solution {
 public:
     int numJewelsInStones(const string &jewels, const string &stones)
     {
-        bool lower[28] = { false };
-        bool upper[28] = { false };
-
-        for(auto u : jewels)
-            if(islower(u)) lower[u - 'a'] = true;
-            else upper[u - 'A'] = true;
-            
+        unordered_map<char, int> x;
+        for (auto i : stones) x[i]++;
         int cnt = 0;
-        for(auto u : stones)
-            if(islower(u)) cnt += lower[u - 'a'];
-            else cnt += upper[u - 'A'];
-
+        for (char i : jewels)
+        {
+            auto k = x.find(i);
+            if(k != x.end()) cnt += k->second;
+        }
         return cnt;
     }
 };
