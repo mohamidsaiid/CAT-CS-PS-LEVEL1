60be0621ba4bfbb201a350ecd235810e96678980
mohamidsaiid <100053790+mohamidsaiid@users.noreply.github.com>
2023-02-13 20:04:13 +0200

    Update Power of Two.cpp

--- Week 4/Power of Two.cpp
+++ Week 4/Power of Two.cpp
@@ -1,3 +1,13 @@
 /*
   problem name : Power of Two
 */
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0) return false; 
+        double x = log2(n);
+        n = log2(n);
+        if ((x-n) > 0) return false;
+        return true;
+    }
+};
