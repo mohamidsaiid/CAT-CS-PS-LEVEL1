2c4d00e905ed44bf976ad81e6b53d9336e8ee2c3
mohamidsaiid <100053790+mohamidsaiid@users.noreply.github.com>
2023-03-01 12:05:36 +0200

    Create Valid Perfect Square.cpp

---
+++ Week 6/Valid Perfect Square.cpp
@@ -0,0 +1,18 @@
+/*
+  problem name : Valid Perfect Square
+*/
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long s = 1;
+        long long e = num;
+        while (s <= e)
+        {
+            long long m = (s+e)/2;
+            if (m*m == num) return true;
+            else if(m*m > num) e = m - 1;
+            else s = m + 1;
+        }
+        return false;
+    }
+};
